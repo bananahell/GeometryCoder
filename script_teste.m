904e8c9695df62e6b93d3a53bece92fedc4d2268
Evaristo <evaristo.ra@hotmail.com>
2020-09-02 12:43:30 -0300

    Adjusting the decoder

--- script_teste.m
+++ script_teste.m
@@ -1,5 +1,5 @@
-% InPC = 'frame0001.ply';
-% prevPC = 'frame0000.ply';
+InPC = 'frame0001.ply';
+prevPC = 'frame0000.ply';
 
 % prevPC = 'C:\Users\evari\Documents\PC_Dataset\sarah9\ply\frame0000.ply';
 % InPC = 'C:\Users\evari\Documents\PC_Dataset\sarah9\ply\frame0001.ply';
@@ -10,8 +10,8 @@
 % prevPC = 'C:\Users\evari\Documents\PC_Dataset_1024\loot\Ply\loot_vox10_1000.ply';
 % InPC = 'C:\Users\evari\Documents\PC_Dataset_1024\loot\Ply\loot_vox10_1001.ply';
 
-prevPC = 'C:\Users\evari\Documents\PC_Dataset_1024\redandblack\Ply\redandblack_vox10_1450.ply';
-InPC = 'C:\Users\evari\Documents\PC_Dataset_1024\redandblack\Ply\redandblack_vox10_1451.ply';
+% prevPC = 'C:\Users\evari\Documents\PC_Dataset_1024\redandblack\Ply\redandblack_vox10_1450.ply';
+% InPC = 'C:\Users\evari\Documents\PC_Dataset_1024\redandblack\Ply\redandblack_vox10_1451.ply';
 
 % prevPC = 'C:\Users\evari\Documents\PC_Dataset_1024\soldier\Ply\soldier_vox10_0536.ply';
 % InPC = 'C:\Users\evari\Documents\PC_Dataset_1024\soldier\Ply\soldier_vox10_0537.ply';
